ab95319832ae75dc811b523e0c8dd79f53ac0260
Abbas Parpia <aparpia03@gmail.com>
2020-02-14 16:10:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,6 @@ def reverse_each_word(sentence)
   empty_array = []
   sentence.split(" , ").each do |word|
     empty_array.unshift(word.reverse)
-    empty_array.reverse
   end
   empty_array.join
 end  
