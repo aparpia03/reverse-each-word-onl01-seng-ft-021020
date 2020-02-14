41299048fbd59f4f6c0a4fa867f45d029eec4894
Abbas Parpia <aparpia03@gmail.com>
2020-02-14 18:28:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   empty_array = [] # create an empty array 
   sentence.split(" , ").each do |word| #split string into array and interate
      #reverse the word so for example Hello becomes olleH
-    empty_array.push(word.reverse) # push that word into the empty_array
+    empty_array << word.reverse # push that word into the empty_array
     # now join it back into one string sentence
   end 
  empty_array.join(" , ")
